18db7d583eb7dec6476fac97c95fddc0c3b2f9c3
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-03-20 21:55:10 +0330

    home work define
    description of home work is on pdf and first classes design

---
+++ film/film.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ASUS on 3/20/2021.
+//
+
+#include "film.h"
---
+++ film/film.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 3/20/2021.
+//
+
+#ifndef FILM_FILM_H
+#define FILM_FILM_H
+
+
+class film {
+        int 
+};
+
+
+#endif //FILM_FILM_H
